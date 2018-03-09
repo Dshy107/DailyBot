04b79f50206dd735be16c697d04e3b9f5353d02b
Gitgest <aable24@gmail.com>
2018-03-09 11:56:10 +0100

    Begun implementing retrival of data from the vejr.eu api.
    Should retrieve information from the vejr.eu api. Just saw it was in the
    wrong file tho lol.

--- MotionSensorController.py
+++ MotionSensorController.py
@@ -3,3 +3,21 @@
 # Current User: Rasmus laptop
 # Date Created: 09/03/2018
 # Developed in PyCharm
+
+#Import requests library
+import requests
+
+# Variables
+#       location
+location = "Roskilde"
+
+#       params dict to be sent to the API
+PARAMS = {'address':location}
+
+#       api-endpoint
+URL = "http://vejr.eu/api.php?location="+location+"&degree=C"
+
+#       quests to be sent and saving the response object
+r = requests.get(url=URL, params=PARAMS)
+
+
