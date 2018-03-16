eab92cf019b45c3113a5e551ab5c234404ec747d
Gitgest <aable24@gmail.com>
2018-03-16 11:05:21 +0100

    Added changes to WeatherAPIController
    We can now recieve weather data from openweathermap.org's api. I have
    saved the key locally on my laptop, the key should later be transferred
    unto the pi where the program should run.

--- WeatherApiController.py
+++ WeatherApiController.py
@@ -3,3 +3,28 @@
 # Current User: Rasmus laptop
 # Date Created: 09/03/2018
 # Developed in PyCharm
+
+# import requests to use the openweathermap.org api
+import requests
+
+# Change app_id value to be a folder on the raspberry pi once the file has been transfered.
+file = open('C:\\Users\\Rasmus laptop\\Keys and IDs for education purposes\\DailyBotWeatherAPI_Key.txt','r')
+api_adress = 'http://api.openweathermap.org/data/2.5/weather?appid='+file.read()+'&q='
+city = input("City Name :")
+
+url = api_adress + city
+
+json_data = requests.get(url).json()
+formatted_data_weather = json_data['weather'][0]['main']
+formatted_data_main = json_data['main']['temp']
+
+# Kelvin to C conversion
+formatted_data_main = formatted_data_main - 273.15
+
+print(json_data)
+print('\n')
+print(formatted_data_weather)
+print('\n')
+print(formatted_data_main)
+
+
