a74a8434035d906bd4b5580d79da92d3f0de9024
Gitgest <aable24@gmail.com>
2018-04-20 08:58:39 +0200

    Implemented a button in the WeatherViewer.py file
    Button added to WeatheViewer.py, the goal of the button is to update the
    GUI with City and get send retrieve data from the weatherAPIController.
    The data should then display in two text fields, one for weather type
    (rain, snow, etc) and one for the temperature.

--- View/WeatherViewer.py
+++ View/WeatherViewer.py
@@ -4,10 +4,12 @@
 # Date Created: 13/04/2018
 # Developed in PyCharm
 
-from tkinter import *
-from Controller import WeatherApiController
-from random import randint
 import tkinter.font
+from tkinter import *
+
+import theme as Theme
+
+from Controller import WeatherApiController
 
 #EARLY DEVELOPMENT, USING: https://stackoverflow.com/questions/43843624/create-textbox-widgets-in-tkinter-of-different-length-for-multiple-labels-in-dif
 
@@ -36,9 +38,15 @@ for n in range(N):
             e.pack(side = 'left')
             # Store the current entrybox reference in "entryboxes"
             entryboxes[frame].append(e)
+    def press():
+        message = 'Button Pressed'
+        win.update()
+
+        b = Button(win, command=press).pack()
 
 # Add some text in the 4th box of the 3rd frame
 entryboxes[frames[0]][1].insert(0,WAC.getTemp())
 
 #Launch the app
+win.configure(background=Theme.GUI_bg)
 win.mainloop()
