f5d3e4e919389e4813d2fc74a5cc8eb1da988b82
Gitgest <aable24@gmail.com>
2018-04-20 11:01:23 +0200

    Added DailyBotMain
    Should be used to initialise the entire program.

---
+++ main/DailyBotMain.py
@@ -0,0 +1,11 @@
+# Project: DailyBot
+# FileName: DailyBotMain
+# Current User: Rasmus laptop
+# Date Created: 20/04/2018
+# Developed in PyCharm
+
+from View import WeatherViewer
+
+WeatherDisplay = WeatherViewer()
+
+WeatherDisplay
